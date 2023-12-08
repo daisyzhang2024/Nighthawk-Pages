9262b5eb329f43fab63b4760741a220e63561bf0
daisyzhang2024 <daisyzhang1257@gmail.com>
2023-12-08 14:06:08 -0800

    mario is back !

--- assets/js/platformer/Player.js
+++ assets/js/platformer/Player.js
@@ -1,5 +1,6 @@
 import GameEnv from './GameEnv.js';
 import Character from './Character.js';
+import Thing1 from './thing.js';
 
 export class Player extends Character{
     // constructors sets up Character object 
