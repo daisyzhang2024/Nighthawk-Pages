af1062f478cf92ef25e1d35e55c5ab38f2898917
daisyzhang2024 <daisyzhang1257@gmail.com>
2024-02-27 13:04:38 -0800

    added memer level elements

---
+++ assets/js/platformer2/Audio5.js
@@ -0,0 +1,10 @@
+//Audio when stepping on Mushroom
+function GirlSound() {
+  const girlSound = document.getElementById("Girl");
+  girlSound.play();
+}
+
+
+//Export Sound
+export default GirlSound
+
